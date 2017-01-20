434a5642b9d1b88cd597f263f1da2f9c473bb11f
tarjei <tarjei.skjaerset@gmail.com>
2017-01-20 19:00:47 +0100

    Added nodejs server

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/bin/bash/nodejs
+
+var http = require("http");
+var fs   = require("fs");
+var url  = require("url");
+
+var app = function(req, res) {};
+
+http.createServer(app).listen(80, function(err) {
+	process.setgid(666);
+	process.setuid(666);
+});
