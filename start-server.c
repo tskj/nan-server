c9e8f78136c4a4be2a3d16e1723120e053f834e6
tarjei <tarjei.skjaerset@gmail.com>
2017-01-20 20:01:25 +0100

    Start server now changes working directory

--- start-server.c
+++ start-server.c
@@ -1,11 +1,22 @@
 #include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
 
 	if (0 != fork()) exit(0);
 
-	signal(SIGCHLD, SIG_IGN);
+	setsid();
 	signal(SIGHUP, SIG_IGN);
+
+	if (0 != fork()) exit(0);
+
+
+	if (-1 == chdir("/www")) {
+		printf("Could not change working directory\n");
+		exit(1);
+	}
 
 	return 0;
 }
