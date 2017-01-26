6e26b806d5dfbe4af939772c825f683305a8c463
tarjei <tarjei.skjaerset@gmail.com>
2017-01-26 12:02:43 +0100

    Made the node server give some debug responses.
    Also added a restart script, and improved the existing kill script
    by using awk.

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var http = require("http");
 var fs   = require("fs");
 var url  = require("url");
 
-var app = function(req, res) {};
+function app(req, res) {
+	res.writeHead(	200
+				 ,	{'Content-Type': 'text/plain'}
+				 );
+	res.end('Dette er Node serveren. Hallo!', 'text');
+}
 
 http.createServer(app).listen(80, function(err) {
 	process.setgid(666);
