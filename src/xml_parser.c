c0b274fc9390c6f791988fd4e52efe92495b614b
tskj <tskj@tarjei.org>
2017-02-01 23:22:10 +0100

    Added test for XML parser.

--- src/xml_parser.c
+++ src/xml_parser.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct e element_t;
+typedef struct n node_t;
+typedef struct a attribute_t;
 
 element_t* get_element(char*, int*);
+char* get_tag(char*, int*);
+attribute_t* get_attribute(char*, int*);
 
-
-typedef struct {
-    element_t element;
+struct n {
+    element_t* element;
     node_t* sibling;
-} node_t;
+};
 
-typedef struct {
+struct a {
     char* key;
     char* value;
     attribute_t* sibling;
-} attribute_t;
+};
 
-typedef struct {
+struct e {
     char* tag;
     attribute_t* attributes;
     node_t* nodes;
     char* text;
-} element_t;
+};
 
 
 int is_whitespace(char c) {
@@ -51,18 +57,18 @@ char* string_concat(char* str, char* start, char* stop) {
     return string;
 }
 
-element_t* parse_xml(char* xml-string) {
+element_t* parse_xml(char* xml_string) {
 
-    if (!xml-string) return NULL;
+    if (!xml_string) return NULL;
 
     int i = 0;
-    while (xml-string[i]) {
-        if (!(xml-string[i+1])) return NULL;
-        if (xml-string[i] == '<') {
-            if (xml-string[i+1] == '!' || xml-string[i+1] == '?') {
+    while (xml_string[i]) {
+        if (!(xml_string[i+1])) return NULL;
+        if (xml_string[i] == '<') {
+            if (xml_string[i+1] == '!' || xml_string[i+1] == '?') {
                 i++;
             } else {
-                return get_element(&xml-string[i], 0);
+                return get_element(&xml_string[i], 0);
             }
         }
     }
@@ -71,7 +77,7 @@ element_t* parse_xml(char* xml-string) {
 element_t* get_element(char* xml, int* last_index) {
 
     if (xml[0] != '<') {
-        printf("Get element needs to start at opening tag\n")
+        printf("Get element needs to start at opening tag\n");
         exit(1);
     }
 
@@ -80,6 +86,7 @@ element_t* get_element(char* xml, int* last_index) {
     int inside_tag = 1;
     attribute_t* at_p = NULL;
     element_t* e_p = NULL;
+    node_t* n_p = NULL;
 
     int j, i = 0;
     while (xml[i]) {
@@ -101,7 +108,7 @@ element_t* get_element(char* xml, int* last_index) {
             if (xml[i] == '/') {
                 e -> text = NULL;
                 e -> nodes = NULL;
-                if (last_index) *last_index = i + 2;
+                if (last_index) *last_index += i + 2;
                 return e;
             } else {
                 at_p = e -> attributes;
@@ -115,18 +122,19 @@ element_t* get_element(char* xml, int* last_index) {
                 if (xml[i+1] == '/') {
                     j = i;
                     while (e -> tag[j]) j++;
-                    if (!strncmp(e.tag, &xml[i+2], j)) {
+                    if (!strncmp(e -> tag, &xml[i+2], j)) {
                         i += j + 2;
-                        if (last_index) *last_index = i;
+                        if (last_index) *last_index += i;
                         return e;
                     } else {
-                        printf("Illegal overlap of tags\nOpen tag: %s\nUnexpected closing tag: %s\n", e.tag, string_concat(NULL, &xml[i+2], &xml[i+2+j]));
+                        printf("Illegal overlap of tags\nOpen tag: %s\nUnexpected closing tag: %s\n", e->tag, string_concat(NULL, &xml[i+2], &xml[i+2+j]));
                         exit(2);
                     }
                 } else {
-                    e_p = e -> nodes;
-                    e -> nodes = get_element(&xml[i], &i);
-                    e -> nodes -> sibling = e_p;
+                    n_p = e -> nodes;
+                    e -> nodes = malloc(sizeof(node_t));
+                    e -> nodes -> element = get_element(&xml[i], &i);
+                    e -> nodes -> sibling = n_p;
                 }
             } else {
                 j = i;
@@ -145,13 +153,13 @@ char* get_tag(char* xml, int* j) {
     memcpy(tag, xml, i);
     tag[i] = 0;
 
-    *j = &xml[i];
+    *j = i;
     return tag;
 }
 
-char* get_attribute(char* xml, int* j) {
+attribute_t* get_attribute(char* xml, int* j) {
 
-    attribute_t at = malloc(sizeof(attribute_t));
+    attribute_t* at = malloc(sizeof(attribute_t));
 
     int i = 0;
     while (xml[i] != '=') i++;
@@ -164,8 +172,8 @@ char* get_attribute(char* xml, int* j) {
     int k = i;
     while (xml[k] != '"') k++;
 
-    at -> value = string_concat(NULL, &xml[i], &xml[k])
+    at -> value = string_concat(NULL, &xml[i], &xml[k]);
 
-    *j = &xml[k + 1];
+    *j = k + 1;
     return at;
 }
---
+++ tests/xml_test.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include "../src/xml_parser.c"
+
+void print_a(attribute_t* at) {
+    if (!at) return;
+    printf("%s=\"%s\" ", at -> key, at -> value);
+    print_a(at -> sibling);
+}
+
+void print_e(element_t* xml, int tab_level) {
+
+    if (!xml) return;
+
+    int i = 0;
+    while (i++ < tab_level) printf("\t");
+
+    printf("%s: ", xml -> tag);
+    print_a(xml -> attributes);
+    printf("\n");
+    i = 0;
+    while (i++ < tab_level+1) printf("\t");
+    printf("%s\n", xml -> text);
+    node_t* n = xml -> nodes;
+    while (n) {
+        print_e(n -> element, tab_level + 1);
+        n = xml -> nodes -> sibling;
+    }
+}
+
+int main() {
+
+    close(0);
+    int fd = open("example.xml", O_RDONLY);
+
+    char buffer[4096];
+    int read_bytes = read(fd, buffer, 4096);
+    buffer[read_bytes] = '\0';
+
+    element_t* AST = parse_xml(buffer);
+    print_e(AST, 0);
+}
