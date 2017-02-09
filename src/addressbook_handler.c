f0f5d031bef30af869d2e9cc0de357a0f1c86d60
tarjei <tarjei.skjaerset@gmail.com>
2017-02-09 19:25:12 +0100

    Implemented PUT request.

--- src/addressbook_handler.c
+++ src/addressbook_handler.c
@@ -284,6 +284,157 @@ void handle_post_request(header_t req) {
     send_header(CREATED, req.request, req.type);
 }
 
+void handle_put_request(header_t req) {
+
+    char* invalid_token;
+    char* start_of_id = &req.path[strlen(ADDRESSBOOK_API)];
+    if (*start_of_id != '/') {
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    start_of_id++;
+
+    int id_to_update = (int) strtol(start_of_id, &invalid_token, 0);
+
+    if ('\0' == *start_of_id) {
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    } else if ('\0' != *invalid_token) {
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    int validation_req = open(VALREQ, O_WRONLY | O_NONBLOCK);
+    if (-1 == validation_req) {
+        send_header(INTERNAL_SERVER_ERROR, req.request, PLAIN);
+        exit(0);
+    }
+
+    int i = 0;
+    while (req.body[i]) {
+        int written_bytes = write(validation_req, &req.body[i], 1);
+        if (1 != written_bytes) {
+            send_header(INTERNAL_SERVER_ERROR, req.request, PLAIN);
+            exit(0);
+        }
+        i++;
+    }
+    close(validation_req);
+
+    char* happy_response = "- - valid";
+
+    int validation_res = open(VALRES, O_RDONLY);
+    if (-1 == validation_res) {
+        send_header(INTERNAL_SERVER_ERROR, req.request, PLAIN);
+        exit(0);
+    }
+
+    char buffer[strlen(happy_response)];
+    int read_bytes = read(validation_res, buffer, sizeof(buffer));
+    if (-1 == read_bytes) {
+        send_header(INTERNAL_SERVER_ERROR, req.request, PLAIN);
+        exit(0);
+    }
+    close(validation_res);
+
+    if (strncmp(happy_response, buffer, sizeof(buffer))) {
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    element_t* contacts = parse_xml(req.body);
+    if (!contacts) {
+        send_header(INTERNAL_SERVER_ERROR, req.request, PLAIN);
+        exit(0);
+    }
+
+    element_t* contact = contacts -> nodes -> element;
+
+    // Make sure there is only one node to be updated
+    if (contacts -> nodes -> sibling != NULL) {
+        send_header(BAD_REQUEST, req.request, PLAIN);
+        exit(0);
+    }
+
+    sqlite3* db;
+    sqlite3_stmt* sql_statement;
+
+    int rc = sqlite3_open(DB_PATH, &db);
+
+    if (rc != SQLITE_OK) {
+        send_header(INTERNAL_SERVER_ERROR, req.request, req.type);
+        exit(0);
+    }
+
+    char* SQL = "UPDATE Addressbook SET ID = ?, Name = ?, Tlf = ? WHERE ID = ?;";
+
+    rc = sqlite3_prepare_v2(db, SQL, -1, &sql_statement, NULL);
+
+    if (rc != SQLITE_OK) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+        send_header(INTERNAL_SERVER_ERROR, req.request, req.type);
+        exit(0);
+    }
+
+    char* invalid_char;
+    char* id_tag = find_text_by_tag(contact, "id");
+    if (!id_tag) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    int id = (int) strtol(id_tag, &invalid_char, 0);
+    if ('\0' != *invalid_char) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    sqlite3_bind_int(sql_statement, 1, id);
+
+    sqlite3_bind_text(sql_statement, 2, find_text_by_tag(contact, "name"), -1, NULL);
+
+    char* tlf = find_text_by_tag(contact, "tlf");
+    if (!tlf) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    sqlite3_bind_text(sql_statement, 3, tlf, -1, NULL);
+
+    sqlite3_bind_int(sql_statement, 4, id_to_update);
+
+    rc = sqlite3_step(sql_statement);
+
+    // ID likely didn't exist
+    if (sqlite3_changes(db) != 1) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+
+        send_header(BAD_REQUEST, req.request, req.type);
+        exit(0);
+    }
+
+    if (rc != SQLITE_DONE) {
+        sqlite3_finalize(sql_statement);
+        sqlite3_close(db);
+        send_header(INTERNAL_SERVER_ERROR, req.request, req.type);
+        exit(0);
+    }
+
+    sqlite3_finalize(sql_statement);
+    sqlite3_close(db);
+
+    send_header(OK, req.request, req.type);
+}
+
 void addressbook_handler(header_t req) {
 
     switch (req.request) {
@@ -294,9 +445,9 @@ void addressbook_handler(header_t req) {
                         break;
         case POST:      handle_post_request(req);
                         break;
-                        /**
         case PUT:       handle_put_request(req);
                         break;
+                        /**
         case DELETE:    handle_delete_request(req);
                         break;
         case ILLEGAL:   send_header(METHOD_NOT_ALLOWED, req.request, req.type);
